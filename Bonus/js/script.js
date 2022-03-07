a195fecb581dc800c65778d587a3afe0f9e5820a
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-07 15:49:37 +0100

    js - undefinied issue

--- Bonus/js/script.js
+++ Bonus/js/script.js
@@ -1,55 +1,46 @@
 const teamContainer = document.querySelector(".team-container");
+const addBtn = document.getElementById("addMemberButton");
+const userName = document.getElementById("name");
+const userRole = document.getElementById("role");
+const userImg = document.getElementById("image");
 
-const users = [
+const users = [];
 
-    {
-        name : 'Wayne Barnett',
-        role : 'Founder & CEO',
-        image : 'img/wayne-barnett-founder-ceo.jpg'
-    },   
-    {
-        name : 'Angela Caroll',
-        role : 'Chied Editor',
-        image : 'img/angela-caroll-chief-editor.jpg'
-    },   
-    {
-        name : 'Walter Gordon',
-        role : 'Office Manager',
-        image : 'img/walter-gordon-office-manager.jpg'
-    },   
-    {
-        name : 'Angela Lopez',
-        role : 'Social Media Manager',
-        image : 'img/angela-lopez-social-media-manager.jpg'
-    },   
-    {
-        name : 'Scott Estrada',
-        role : 'Developer',
-        image : 'img/scott-estrada-developer.jpg'
-    },   
-    {
-        name : 'Barbara Ramos',
-        role : 'Graphic Designer',
-        image : 'img/barbara-ramos-graphic-designer.jpg'
-    }, 
+addBtn.addEventListener('click', function() {
+    let inputName = userName.value;
+    let inputRole = userRole.value;
+    let inputImg = userImg.value;
 
-];
+    console.log(inputName, inputRole, inputImg);
 
-for (let i = 0; i < users.length; i++) { 
+    var user = {};
+    user.name = inputName;
+    user.role = inputRole;
+    user.image = inputImg;
+    users.push({user: user});
 
-    let teamCard = document.createElement("div");
-    teamCard.classList.add("team-card")
-    teamCard.innerHTML = 
-    `<div class="card-image">
-        <img
-          src="${users[i].image}"
-          alt="${users[i].name}"
-        />
-    </div>
-    <div class="card-text">
-        <h3>${users[i].name}</h3>
-        <p>${users[i].role}</p>
-    </div>`;
+    console.log(users);
 
-    teamContainer.append(teamCard);
-}
+    for (let i = 0; i < users.length; i++) { 
+
+        let teamCard = document.createElement("div");
+        teamCard.classList.add("team-card")
+        teamCard.innerHTML =`<div class="card-image">
+            <img
+              src="${users[i].image}"
+              alt="${users[i].name}"
+            />
+        </div>
+        <div class="card-text">
+            <h3>${users[i].name}</h3>
+            <p>${users[i].role}</p>
+        </div>`;
+    
+        teamContainer.append(teamCard);
+    }
+
+});
+
+
+// ogni volta che clicco le cose messe dentro l'input vadano aggiunte al obj dentro array
+
